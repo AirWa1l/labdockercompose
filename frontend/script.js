fc1ec27848966cbfedd29ae1ec1f86bd87506561
Mausterl26 <jhoansebitas27@gmail.com>
2025-03-23 17:58:52 -0500

    added service 4

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         { name: "Service 1", url: "http://localhost:5001/api" },
         { name: "Service 2", url: "http://localhost:5002/api" },
         { name: "Service 3", url: "http://localhost:5003/api" },
+        { name: "Service 4", url: "http://localhost:5004/api"},
         { name: "service 8", url: "http://localhost:5008/api"},
         { name: "Service 7", url: "http://localhost:5007/api"},
         { name: "Service 01", url: "http://localhost:5011/api" }
